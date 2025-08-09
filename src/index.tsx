14bb2d51bf929413a4a9b0ee808340cb3f511946
Umair Ashraf <umrashrf@gmail.com>
2025-08-09 11:10:44 -0600

    Fix build issues after converting to preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from 'react'
+import React from 'preact'
+import { useState, useRef } from 'preact/hooks';
 import styles from './styles.module.css'
 
 interface Props {
@@ -19,31 +20,31 @@ export interface Result {
   lon: string
 }
 
-export class debouncedMethod<T>{
-  constructor(method:T, debounceTime:number){
+export class debouncedMethod<T> {
+  constructor(method: T, debounceTime: number) {
     this._method = method;
     this._debounceTime = debounceTime;
   }
-  private _method:T;
-  private _timeout:number;
-  private _debounceTime:number;
-  public invoke:T = ((...args:any[])=>{
+  private _method: T;
+  private _timeout: number;
+  private _debounceTime: number;
+  public invoke: T = ((...args: any[]) => {
     this._timeout && window.clearTimeout(this._timeout);
-    this._timeout = window.setTimeout(()=>{
+    this._timeout = window.setTimeout(() => {
       (this._method as any)(...args);
-    },this._debounceTime);
+    }, this._debounceTime);
   }) as any;
 }
 
-const renderResults = (results: any, callback: Function | undefined, setShowResults:React.Dispatch<React.SetStateAction<boolean>>) => 
+const renderResults = (results: any, callback: Function | undefined, dispatch: (value: boolean) => void) =>
   <div className={styles.results}>
-    {results.map((result: Result, index: number) => 
-      <div key={index} className={styles.result} onClick={()=> {
-        if(callback) {
+    {results.map((result: Result, index: number) =>
+      <div key={index} className={styles.result} onClick={() => {
+        if (callback) {
           callback(result);
-          setShowResults(false);
+          dispatch(false);
         }
-        }}>
+      }}>
         {result?.display_name}
       </div>
     )}
@@ -55,51 +56,55 @@ export const ReactOsmGeocoding = ({ placeholder = "Enter address", debounce = 10
   const [showResults, setShowResults] = useState(false);
   const [showLoader, setShowLoader] = useState(false);
   const mainContainerRef = useRef<HTMLDivElement>(null);
-  
-  document.addEventListener('click', function(event) {
-      var isClickInside = mainContainerRef?.current?.contains(event.target as Node);
-      if (!isClickInside) {
-        setShowResults(false);
-      }
+
+  document.addEventListener('click', function (event) {
+    var isClickInside = mainContainerRef?.current?.contains(event.target as Node);
+    if (!isClickInside) {
+      setShowResults(false);
+    }
   });
 
-  document.onkeyup = function(event) {
-    if (event.key === "Escape"){
+  document.onkeyup = function (event) {
+    if (event.key === "Escape") {
       setShowResults(false);
-      }
+    }
   }
 
   function getGeocoding(address = "") {
-    if(address.length === 0) return;
+    if (address.length === 0) return;
 
     setShowLoader(true);
 
     let url = `https://nominatim.openstreetmap.org/search?format=json&q=${address}&city=${city}&countrycodes=${countrycodes}&accept-language=${acceptLanguage}`;
 
-    if(viewbox.length)
+    if (viewbox.length)
       url = `${url}&viewbox=${viewbox}&bounded=1`;
 
     fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      setResults(data);
-      setShowResults(true);
-    })
-    .catch(err =>console.warn(err))
-    .finally(() => setShowLoader(false));
+      .then(response => response.json())
+      .then((data) => {
+        setResults(data);
+        setShowResults(true);
+      })
+      .catch(err => console.warn(err))
+      .finally(() => setShowLoader(false));
   }
 
-  var debouncer = new debouncedMethod((address: string)=>{
+  var debouncer = new debouncedMethod((address: string) => {
     getGeocoding(address);
-   }, debounce);
+  }, debounce);
 
 
 
   return <div className={styles.reactOsmGeocoding} ref={mainContainerRef}>
-    <input type="text" name="geocoding" id="geocoding" placeholder={placeholder} 
+    <input type="text" name="geocoding" id="geocoding" placeholder={placeholder}
       onClick={() => setShowResults(true)}
-      onChange={event => debouncer.invoke(event.target.value)}/>
-    <img src={iconUrl} width={"30px"} height={"30px"}/>
+      onChange={event => {
+        const target = event.target as HTMLTextAreaElement;
+        debouncer.invoke(target.value);
+      }
+      } />
+    <img src={iconUrl} width={"30px"} height={"30px"} />
     {showLoader && <div className={styles.loader}></div>}
     {(results.length && showResults) ? renderResults(results, callback, setShowResults) : ""}
   </div>
